64e87b575b3c362e1dc6e897ad33472995bbb20b
maximeabade <maximeabade>
2024-02-25 12:53:05 +0100

    edited paths to my local machine

--- main.py
+++ main.py
@@ -9,20 +9,20 @@ def main():
     learn()
     result = capture_et_reconnaissance()
 
-    os.system("rm ./images/temp/*")
+    os.system("rm /home/max/Bureau/Work/Perso/PROJECTS/FaceRecon/images/temp/*")
 
     if not result:
-        print("Intrus détecté")
+        ##print("Intrus détecté")
         ## for 10 seconds play the alarm sound
         start_time = time.time()
         while time.time() - start_time < 10:
-            playsound.playsound('./intrus.mp3', True)
+            playsound.playsound('/home/max/Bureau/Work/Perso/PROJECTS/FaceRecon/intrus.mp3', True)
         ## close the sound
         
         return
     else:
-        print("Bienvenue à la maison, Maxime !")
-        playsound.playsound('./bonjour.mp3', True)
+        ##print("Bienvenue à la maison, Maxime !")
+        playsound.playsound('/home/max/Bureau/Work/Perso/PROJECTS/FaceRecon/bonjour.mp3', True)
 
 if __name__ == "__main__":
     main()
